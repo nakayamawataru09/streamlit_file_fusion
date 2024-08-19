e492e912ebbd7aad14c7cce843677bf149430fd9
exture-nakayama <103922377+nakayamawataru09@users.noreply.github.com>
2024-08-19 11:43:59 +0900

    Update streamlit_app.py
    jpgを許容

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,23 @@ from PIL import Image
 import io
 
 # アプリのタイトル
-st.title('PDFとPNGファイルの結合アプリ')
+st.title('PDFと画像ファイルの結合アプリ')
 
 # ファイルアップロード機能
-uploaded_files = st.file_uploader("PDFとPNGファイルをアップロードしてください", type=["pdf", "png"], accept_multiple_files=True)
+uploaded_files = st.file_uploader("PDF、PNG、JPGファイルをアップロードしてください", type=["pdf", "png", "jpg"], accept_multiple_files=True)
 
 pdf_files = []
-png_files = []
+image_files = []
 
 if uploaded_files:
     # アップロードされたファイルを分類
     for uploaded_file in uploaded_files:
         if uploaded_file.type == "application/pdf":
             pdf_files.append(uploaded_file)
-        elif uploaded_file.type == "image/png":
-            png_files.append(uploaded_file)
+        elif uploaded_file.type in ["image/png", "image/jpeg"]:
+            image_files.append(uploaded_file)
 
-    if pdf_files and png_files:
+    if pdf_files and image_files:
         # 新しいPDFドキュメントを作成
         output_pdf = fitz.open()
 
@@ -30,9 +30,9 @@ if uploaded_files:
             output_pdf.insert_pdf(pdf_doc)
             pdf_doc.close()
 
-        # PNGファイルをPDFに変換して追加
-        for png_file in png_files:
-            image = Image.open(png_file)
+        # 画像ファイルをPDFに変換して追加
+        for image_file in image_files:
+            image = Image.open(image_file)
             pdf_bytes = io.BytesIO()
             image.save(pdf_bytes, format="PDF")
             image_pdf = fitz.open("pdf", pdf_bytes.getvalue())
@@ -52,5 +52,4 @@ if uploaded_files:
             mime="application/pdf"
         )
     else:
-        st.warning("PDFファイルとPNGファイルの両方をアップロードしてください。")
-
+        st.warning("PDFファイルと画像ファイルの両方をアップロードしてください。")
